3dc40b3e84b8524e9f244a87334ca6ffc53da684
s144122 <s144122@student.dtu.dk>
2017-04-23 21:44:33 +0200

    2 og 3
    Halvdelen af nr. 2 og det meste af nr 3

---
+++ ContexFree/while_language/ast/LargerExpr.java
@@ -0,0 +1,19 @@
+package while_language.ast;
+import while_language.Environment;
+import while_language.VariableNotDefinedException;
+
+public class LargerExpr extends BoolExpr  {
+	private ArithExpr expression1;
+	private ArithExpr expression2;
+	
+	public LargerExpr(ArithExpr expression1, ArithExpr expression2) {
+		this.expression1 = expression1;
+		this.expression2 = expression2;
+	}
+	
+	@Override
+	public boolean evaluate(Environment env) throws VariableNotDefinedException {
+		return expression1.evaluate(env) <= expression2.evaluate(env);
+	}
+
+}
