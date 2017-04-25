11372823183739a362a3db651518f1562f10470b
s144122 <s144122@student.dtu.dk>
2017-04-25 13:01:20 +0200

    3

--- ContexFree/while_language/ast/WhileLoop.java
+++ ContexFree/while_language/ast/WhileLoop.java
@@ -1,5 +1,24 @@
 package while_language.ast;
 
-public class WhileLoop {
+import while_language.Environment;
+import while_language.VariableNotDefinedException;
+
+public class WhileLoop extends Statement {
+
+	private BoolExpr condition;
+	private Statement whileV;
+
+	public WhileLoop(BoolExpr condition, Statement whileV) {
+		this.condition = condition;
+		this.whileV = whileV;
+
+	}
+
+	@Override
+	public void evaluate(Environment env) throws VariableNotDefinedException {
+		while (condition.evaluate(env)) {
+			whileV.evaluate(env);	
+		}
+	}
 
 }
